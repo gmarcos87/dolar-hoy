0c21181c105d022b0b4b76efae992a5dcf863ac5
Marcos Gutierrez <gmarcos87@gmail.com>
2017-03-15 13:20:50 -0300

    Add test

---
+++ spec/index.spec.js
@@ -0,0 +1,22 @@
+var app = require("../index.js");
+
+describe("Test app", function() {
+
+  it("get a request", function(done) {
+    app.init('http://ws.geeklab.com.ar/dolar/get-dolar-json.php');
+    app.fetch((res)=>{
+      expect(res.libre).toBeDefined();
+      expect(res.blue).toBeDefined();
+      done();
+    });
+  });
+
+  it("get an error", function(done) {
+    app.init('http://fake-url/dolar/get-dolar-json.php');
+    app.fetch((res,err)=>{
+      expect(err).toBeDefined();
+      done();
+    });
+
+  });
+});
